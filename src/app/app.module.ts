36dafe39b4c7182851508710563377f5b089a446
zmyzheng <zhengzmy@gmail.com>
2017-07-22 19:40:27 -0500

    assginment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { DishService } from './services/dish.service';
 import {PromotionService} from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import {PromotionService} from './services/promotion.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
